e2e3c09354e905d7cad07bca3d16276b49755dec
amrameen769 <amrameen769@gmail.com>
2020-07-26 09:01:27 +0530

    first commit

---
+++ init.php
@@ -0,0 +1,37 @@
+<?php
+// InitFile
+
+// Add Reusable Directory Autoloader
+// Class Autoloader will detect all the subdirectories and add the class files followed under "/lib" directory on your server root.
+// PHP 7.2^ preferred method
+
+function requireClass($classFile, $className)
+{
+    if (!class_exists($className)) require_once $classFile;
+}
+
+function retrieveDir($dirs, $currentPath, $className)
+{
+    foreach ($dirs as $dir) {
+        if ($dir == ".." || $dir == ".") {
+            continue;
+        }
+        $newPath = $currentPath . '/' . $dir;
+        if (is_dir($newPath)) {
+            $subdirs = scandir($newPath);
+            retrieveDir($subdirs, $newPath, $className);
+        } else {
+            requireClass($newPath, $className);
+        }
+    }
+}
+
+if (!function_exists('autoClassLoader')) {
+    function autoClassLoader($className)
+    {
+        $subdirs = scandir($_SERVER['DOCUMENT_ROOT'] . '/lib');
+        $classFile = retrieveDir($subdirs, $_SERVER['DOCUMENT_ROOT'] . '/lib', $className);
+    }
+}
+
+spl_autoload_register('autoClassLoader');
